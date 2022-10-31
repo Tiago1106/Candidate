67ba3cd9943d0ea4f6d2efd5e8e86f863ac2edc0
Tiago1106 <tiagofreitasp00@gmail.com>
2022-10-31 16:08:16 -0300

    feat: add pages

---
+++ src/pages/finish/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function finish() {
+  return (
+    <div>
+      <span>Finish</span>
+    </div>
+  );
+}
+
+export default finish;
---
+++ src/pages/login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function login() {
+  return (
+    <div>
+      <span>Login</span>
+    </div>
+  );
+}
+
+export default login;
